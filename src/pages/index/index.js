a2c4a175f22d9fb7f104f157d814e84c18752ba3
何明胜 <husen@hemingsheng.cn>
2019-07-02 02:02:00 +0800

    1.修复依赖boostrap失败;

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -3,11 +3,10 @@
 import Vue from 'vue'
 import VueResource from 'vue-resource'
 
-import 'jquery'
-import 'bootstrap'
-
 import Skill6 from './Skill6'
 import router from './router'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
