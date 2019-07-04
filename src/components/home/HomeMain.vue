017c0cfd5ffde0b5f03a591bdeb485564733c525
何明胜 <husen@hemingsheng.cn>
2019-07-05 03:00:01 +0800

    1.还原首页左中右布局,日历放到右边栏;

--- src/components/home/HomeMain.vue
+++ src/components/home/HomeMain.vue
@@ -12,6 +12,6 @@ export default {
 
 <style scoped>
 .home-main {
-  margin: auto 20%;
+  width: 56%;
 }
 </style>
--- src/components/home/HomeRightSide.vue
+++ src/components/home/HomeRightSide.vue
@@ -4,18 +4,28 @@
       <el-row class="hot-label-row">
         <el-button type="primary" size="mini">Java</el-button>
         <el-button type="primary" size="mini">全栈</el-button>
+        <el-button type="primary" size="mini">VUE</el-button>
+        <el-button type="primary" size="mini">深度学习</el-button>
       </el-row>
       <el-row class="hot-label-row">
         <el-button type="primary" size="mini">VUE</el-button>
         <el-button type="primary" size="mini">人工智能</el-button>
       </el-row>
     </div>
+    <div>
+      <lunar-calendar></lunar-calendar>
+    </div>
   </div>
 </template>
 
 <script>
+import LunarCalendar from './sidebar/LunarCalendar'
+
 export default {
-  name: 'HomeRightSide'
+  name: 'HomeRightSide',
+  components: {
+    LunarCalendar
+  }
 }
 </script>
 
@@ -23,13 +33,15 @@ export default {
 .info-flow-side-right {
   position: relative;
   display: block;
-  width: 20%;
+  width: 24%;
   background-color: whitesmoke;
   margin-right: 15px;
 }
 
 .hot-label {
   margin: 10px;
+  background-color: white;
+  padding: 5px;
 }
 
 .hot-label-row {
