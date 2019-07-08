4018b7da67b1fd9c466fd7e9390989a4c5ab27d3
何明胜 <husen@hemingsheng.cn>
2019-07-09 00:02:37 +0800

    1.信息流增加加载更多;

--- src/components/infoflow/center/LatestUpdate.vue
+++ src/components/infoflow/center/LatestUpdate.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
-    <div v-for="index in 10" :key="index">
+    <div v-for="index in count" :key="index">
       <single-update></single-update>
+    </div>
+    <div class="load-more">
+      <Button long v-if="page <= page_count" @click="loadMore" class="load-btn">点击加载更多</Button>
+      <Button long v-else class="load-btn">没有更多了</Button>
     </div>
   </div>
 </template>
@@ -10,12 +14,33 @@
 import SingleUpdate from './single/SingleUpdate'
 
 export default {
-  name: 'LatestUpdate',
   components: {
     SingleUpdate
+  },
+  data () {
+    return {
+      count: 4,
+      page: 1,
+      page_count: 3
+    }
+  },
+  methods: {
+    loadMore () {
+      this.count += 2
+      this.page += 1
+    }
   }
 }
 </script>
 
 <style scoped>
+.load-more {
+  width: 98%;
+  margin: 10px auto;
+}
+
+.load-btn {
+  font-size: 15px;
+  font-weight: 600;
+}
 </style>
