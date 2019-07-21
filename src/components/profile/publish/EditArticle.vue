c5f4b58b8b7019e9f75b7b1168b58c32f584e592
何明胜 <husen@hemingsheng.cn>
2019-07-22 01:00:16 +0800

    1.文章增加其他信息;

--- src/components/profile/publish/EditArticle.vue
+++ src/components/profile/publish/EditArticle.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <!-- 顶部 -->
     <div class="top-menu">
       <el-row>
         <el-col :span="2" :offset="10">
@@ -23,6 +24,33 @@
       </el-row>
     </div>
     <el-divider></el-divider>
+    <!-- 其他信息 -->
+    <div class="article-info">
+      <el-row>
+        <el-col :span="20" :offset="2">
+          <el-input placeholder="请输入标题" v-model="articleTitle" clearable></el-input>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="20" :offset="2">
+          <el-input
+            type="textarea"
+            :autosize="{ minRows: 2, maxRows: 6}"
+            placeholder="请输入摘要"
+            v-model="articleDesc"
+          ></el-input>
+        </el-col>
+      </el-row>
+      <el-row :gutter="20">
+        <el-col :span="3" :offset="2">
+          <el-input placeholder="请输入文章标签" v-model="articleLabel" clearable></el-input>
+        </el-col>
+        <el-col :span="3">
+          <el-input placeholder="请输入文章分类" v-model="articleCategory" clearable></el-input>
+        </el-col>
+      </el-row>
+    </div>
+    <!-- 编辑器 -->
     <div class="editor">
       <mavon-editor v-model="value" />
     </div>
@@ -32,7 +60,13 @@
 <script>
 export default {
   data () {
-    return { value: '' }
+    return {
+      value: '',
+      articleTitle: '',
+      articleDesc: '',
+      articleLabel: '',
+      articleCategory: ''
+    }
   }
 }
 </script>
@@ -47,8 +81,17 @@ export default {
   float: right;
 }
 
+.article-info {
+  width: 96%;
+  margin: 0 auto;
+}
+
 .editor {
   width: 96%;
   margin: 20px auto;
+}
+
+.el-row {
+  margin-bottom: 20px;
 }
 </style>
